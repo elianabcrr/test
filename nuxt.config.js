f3d92ff1c8bdb6c0c7f04286c0e7b2ba0473871f
elianabcrr <eliana.becerra0916@gmail.com>
2022-12-15 10:54:43 -0400

    cookiebot updated 7

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,11 +34,7 @@ export default {
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200&display=swap'}
     ], 
     scripts: [
-      {
-        id:"CookieDeclaration",
-        src:"https://consent.cookiebot.com/95bd0f51-1f5e-484d-89ca-0ef9c12c9640/cd.js",
-        type:"text/javascript"
-      }
+   
     ],
     
   },
